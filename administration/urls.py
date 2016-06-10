8d3ceb2ad1ab9704f9cce253bc1a52479c75fb3a
Rulx Philome ALEXIS <rulxphilome.alexis@gmail.com>
2016-06-10 10:50:26 -0500

    Fixing Ciec Templates

--- administration/urls.py
+++ administration/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('administration.views',
     url(r'^$','adm_home',name="adm_home"),
+    url(r'^liste_instances/$','instances',name="instances"),
+    url(r'^liste_candidats/$','candidats',name="candidats"),
+    url(r'^csv_exporting/$','csv_exporting',name="csv_exporting"),
+    url(r'^csv_exporting_2/$','csv_exporting_2',name="csv_exporting_2"),
 )
--- administration/views.py
+++ administration/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from annuaire.models import *
+from ciec_portail.models import *
+import csv
 # Create your views here.
 def adm_home(request):
     return render(request,'home_administration.html',{})
+
+def instances(request):
+    cdts = InstanceCandidature.objects.all()
+    return render(request,'liste_instence.html',{'l_cndts':cdts})
+
+def candidats(request):
+    cdts = CandidatMembre.objects.all()
+    return render(request,'liste_candidats.html',{'l_cndts':cdts})
+
+def csv_exporting(request):
+    # td = vari
+    cdts = InstanceCandidature.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachement;filename="liste_instances.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['Nom','Prenom','Sexe'])
+    for moi in cdts:
+        writer.writerow([moi.nom,moi.prenom,moi.sexe])
+    return response
+
+def csv_exporting_2(request):
+    # td = vari
+    cdts = CandidatMembre.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachement;filename="listes_membres.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['Nom','Prenom','Sexe'])
+    for moi in cdts:
+        writer.writerow([moi.nom,moi.prenom,moi.sexe])
+    return response
